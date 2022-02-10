ff3368806384932d4c6dde9949987feb1de834ce
ilyasweb <ilyasweb0721@gmail.com>
2022-02-10 14:03:36 -0500

    Added mysql and commented out

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
-print(""Added from github)
+# import mysql.connector
+#
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="1234",
+#   database="mydb"
+# )
+#
+# my_cursor = mydb.cursor()
+#
+# my_cursor.execute('select * from students')
+#
+# results = my_cursor.fetchall()
+#
+# for i in results:
+#     print(i)
+
+
