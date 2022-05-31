1fa6b182a3b463643248ceaa0b61b16330476243
ilyasweb <ilyasweb0721@gmail.com>
2022-05-31 01:33:32 -0400

    uncommented the code

--- demo.py
+++ demo.py
@@ -1,19 +1,19 @@
-# import mysql.connector
-#
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="1234",
-#   database="mydb"
-# )
-#
-# my_cursor = mydb.cursor()
-#
-# my_cursor.execute('select * from students')
-#
-# results = my_cursor.fetchall()
-#
-# for i in results:
-#     print(i)
+import mysql.connector
 
-# This info is added from the server. 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1234",
+  database="mydb"
+)
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute('select * from students')
+
+results = my_cursor.fetchall()
+
+for i in results:
+    print(i)
+
+
